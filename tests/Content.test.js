b65d6e6f2a6755ea8d0da286adf183eaee315721
Bryan <bryan@punknown.net>
2025-02-15 17:14:18 +0100

    test: add unit tests for Content bootstrap and challenge type registration

---
+++ tests/Content.test.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import { ChallengeFactory } from "../src/ChallengeFactory.js";
+import "../src/content.js";
+
+describe("Content bootstrap", () => {
+	test("registers 'translate' and 'tapComplete' challenge types", () => {
+		expect(ChallengeFactory.registry.has("translate")).toBe(true);
+		expect(ChallengeFactory.registry.has("tapComplete")).toBe(true);
+	});
+});
