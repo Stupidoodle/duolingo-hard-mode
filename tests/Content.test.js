af4fbc70e9492c8cb5f6085622410d239ebdbf40
Bryan <bryan@punknown.net>
2025-02-17 01:15:10 +0100

    test: add 'listenTap' challenge type registration to Content tests

--- tests/Content.test.js
+++ tests/Content.test.js
@@ -8,5 +8,6 @@ describe("Content bootstrap", () => {
 	test("registers 'translate' and 'tapComplete' challenge types", () => {
 		expect(ChallengeFactory.registry.has("translate")).toBe(true);
 		expect(ChallengeFactory.registry.has("tapComplete")).toBe(true);
+		expect(ChallengeFactory.registry.has("listenTap")).toBe(true);
 	});
 });
